3c1c185d4ab97aa0375e6a725ba7c714e6f65abb
Timotej <20747466+Timic3@users.noreply.github.com>
2020-10-31 21:59:03 +0100

    Initial push of libGDX project

---
+++ android/src/com/intospace/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.intospace.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.intospace.game.IntoSpaceGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IntoSpaceGame(), config);
+	}
+}
---
+++ desktop/src/com/intospace/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.intospace.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.intospace.game.IntoSpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IntoSpaceGame(), config);
+	}
+}
