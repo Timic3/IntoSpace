b38a13927ea09833d14250748252ee9472fdc30a
Timotej <20747466+Timic3@users.noreply.github.com>
2021-07-24 19:17:54 +0200

    Use LWJGL 3 as a new backend

--- desktop/src/com/intospace/game/desktop/DesktopLauncher.java
+++ desktop/src/com/intospace/game/desktop/DesktopLauncher.java
@@ -1,18 +1,19 @@
 package com.intospace.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.intospace.game.IntoSpaceGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 720;
-		config.backgroundFPS = 60;
-		config.foregroundFPS = 60;
-		config.title = "Into Space";
-		config.forceExit = false;
-		new LwjglApplication(new IntoSpaceGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Into Space");
+		config.setWindowedMode(1280, 720);
+		config.setResizable(false);
+		config.useVsync(true);
+		// config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+		config.setForegroundFPS(60);
+		config.setIdleFPS(60);
+		new Lwjgl3Application(new IntoSpaceGame(), config);
 	}
 }
